0e07c0abd27c8c1ecb2f85a0f4192efc201c8253
連照銘 <cm.lien.go@gmail.com>
2026-05-14 10:21:09 +0800

    修改二

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/__break_brick/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
